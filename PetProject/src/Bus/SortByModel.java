2e84bc83c4e5e6f80687b1925c682663d0f1171e
SorokinDenis <ms.sorok400@mail.ru>
2026-05-11 11:03:11 +0300

    Update SortByModel.java
    изменение сортировки модели

--- PetProject/src/Bus/SortByModel.java
+++ PetProject/src/Bus/SortByModel.java
@@ -1,7 +1,21 @@
-package Bus;
+package bus;
 
-class SortByModel implements SortStrategy {
-    public int compare(Bus b1, Bus b2) {
-        return b1.getModel().compareTo(b2.getModel());
+public class SortByModel implements SortStrategy {
+
+    @Override
+    public void sort(Bus[] buses) {
+
+        for (int i = 0; i < buses.length - 1; i++) {
+
+            for (int j = 0; j < buses.length - i - 1; j++) {
+
+                if (buses[j].getModel().compareTo(buses[j + 1].getModel()) > 0) {
+
+                    Bus temp = buses[j];
+                    buses[j] = buses[j + 1];
+                    buses[j + 1] = temp;
+                }
+            }
+        }
     }
 }
